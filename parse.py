9102cdb55b840bc70e8eda76b142046c4ecf4459
a.nishioka0817 <61805838+anishioka0817@users.noreply.github.com>
2021-10-14 22:13:03 +0900

    OfferIDをパースする処理を追加する

--- parse.py
+++ parse.py
@@ -1,11 +1,20 @@
 from soup import get_html_text
+from so import get_back, get_forward
 
 def parse_html_text(html_text):
     soup = get_html_text(html_text)
+    get_offer_id(soup)
     get_company_name(soup)
     #for each in soup.find_all("a"):
         #print(each.text)
     return
+
+def get_offer_id(soup):
+    for each in soup.find_all("h4", class_="c-job_offer-recruiter__name"):
+        href = each.find("a").get("href")
+        back = get_back("=", href)
+        id = get_forward("#", back)
+        print(id)
 
 def get_company_name(soup):
     for each in soup.find_all("h4", class_="c-job_offer-recruiter__name"):
