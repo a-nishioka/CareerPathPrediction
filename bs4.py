15454c9520c5d00ce123f2bd5160e8a9ec81f998
a.nishioka0817 <61805838+anishioka0817@users.noreply.github.com>
2021-10-14 21:47:05 +0900

    企業名の取得処理を追加

--- bs4.py
+++
@@ -1,5 +0,0 @@
-from bs4 import BeautifulSoup
-
-
-def get_html_text(html):
-    return BeautifulSoup(html, 'html.parser')
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from scrape import scrape_site
+from parse import parse_html_text
 
 html = scrape_site("https://paiza.jp/career/job_offers")
 
 # 分析用コード
 print(html.text)
+
+parse_html_text(html.text)
