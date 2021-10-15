f562e19a1759528ac9480753f08d8fdafe9c57de
a.nishioka0817 <61805838+anishioka0817@users.noreply.github.com>
2021-10-16 02:08:27 +0900

    不要な引数を削除

--- Data.py
+++ Data.py
@@ -13,6 +13,6 @@ class Data:
     def insert_company_name(self, offer_id_list, company_name_list):
         self.db.insert("train_company_name", "offer_id", "company_name", offer_id_list, company_name_list)
         
-    def truncate_company_name(self, offer_id_list, company_name_list):
+    def truncate_company_name(self):
         self.db.truncate("train_company_name")
 
