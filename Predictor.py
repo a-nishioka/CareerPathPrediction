a0dfbae89b4f5cc62effc472e6a786bca3240a3a
a.nishioka0817 <61805838+anishioka0817@users.noreply.github.com>
2021-10-30 14:35:22 +0900

    不要なPrintを削除する

--- Predictor.py
+++ Predictor.py
@@ -21,9 +21,6 @@ enviroment_unique_list = pretreatment.get_unique_list(environment_ele_list)
 framework_ele_list = pretreatment.get_element_list(dataset, data.framework)
 framework_unique_list = pretreatment.get_unique_list(framework_ele_list)
 
-for text in framework_unique_list:
-    print(text)
-
 del data
 del analysis
 del pretreatment
