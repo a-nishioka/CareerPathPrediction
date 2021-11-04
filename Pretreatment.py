d922e1adae21d9463daa92a54e0f1032637d5baa
a.nishioka0817 <61805838+anishioka0817@users.noreply.github.com>
2021-11-04 19:20:25 +0900

    fill_zerosの修正

--- Pretreatment.py
+++ Pretreatment.py
@@ -15,8 +15,9 @@ class Pretreatment:
         return
 
     def fill_zeros(self, dataframe, column_name, value):
-        dataframe[column_name].replace(0, value)
-
+        for i in range(len(dataframe[column_name])):
+            if(dataframe[column_name][i] == 0):
+                dataframe[column_name][i] = value
 
     def fill_none(self, dataframe, column_name, text):
         for i in range(len(dataframe[column_name])):
